fe887f0e913c6def4bfda4f29793b93308a832a9
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-02-10 20:48:49 -0300

    refactor: Update repositories and add JPQL query

--- src/main/java/io/github/bragabriel/library_api/repository/AuthorRepository.java
+++ src/main/java/io/github/bragabriel/library_api/repository/AuthorRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * @see AuthorRepositoryTest
+ */
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
 }
--- src/main/java/io/github/bragabriel/library_api/repository/BookRepository.java
+++ src/main/java/io/github/bragabriel/library_api/repository/BookRepository.java
@@ -4,17 +4,27 @@ import io.github.bragabriel.library_api.model.Author;
 import io.github.bragabriel.library_api.model.Book;
 import io.github.bragabriel.library_api.model.BookGenreEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @see BookRepositoryTest
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    //JPA Query Methods
     List<Book> findByAuthor(Author author);
 
     List<Book> findByTitleAndPrice(String title, BigDecimal price);
 
     List<Book> findByTitleOrGenre(String title, BookGenreEnum genre);
+
+    //JPQL
+    @Query("select b from Book as b order by b.title, b.price")
+    List<Book> listAllOrderedByTitleAndPrice();
 }
