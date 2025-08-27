a60f90e1b05eda550fda6b12bfac9c623bbe512d
gabriel-braga <gabriel.bragasilva@hotmail.com>
2025-08-27 10:44:55 -0300

    chore(db): update data.sql script

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,7 +2,10 @@ create table author(
 	id uuid not null primary key,
 	name varchar(100) not null,
 	birthdate date not null,
-	nationality varchar(50) not null
+	nationality varchar(50) not null,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    user_id UUID
 );
 
 create table book(
@@ -12,6 +15,9 @@ create table book(
 	publication_date date not null,
 	genre varchar(30) not null,
 	price numeric(18,2),
+	created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    user_id UUID,
 	id_author uuid not null references author(id)
 	constraint chk_genre check(genre in ('FICTION', 'FANTASY', 'MYSTERY', 'ROMANCE', 'BIOGRAPHY', 'SCIENCE'))
 );
