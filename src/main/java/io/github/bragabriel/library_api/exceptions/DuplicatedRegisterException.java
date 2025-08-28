db906a6860475b77c725af164c8001a9c566f9dd
gabriel-braga <gabriel.bragasilva@hotmail.com>
2025-08-28 18:47:37 -0300

    feat: add custom exceptions

---
+++ src/main/java/io/github/bragabriel/library_api/exceptions/DuplicatedRegisterException.java
@@ -0,0 +1,7 @@
+package io.github.bragabriel.library_api.exceptions;
+
+public class DuplicatedRegisterException extends RuntimeException{
+	public DuplicatedRegisterException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/io/github/bragabriel/library_api/exceptions/NotAllowedException.java
@@ -0,0 +1,7 @@
+package io.github.bragabriel.library_api.exceptions;
+
+public class NotAllowedException extends RuntimeException {
+	public NotAllowedException(String message) {
+		super(message);
+	}
+}
